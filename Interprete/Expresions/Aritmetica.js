1f0865d3d2ec04474d1f31a8c84c96288bde1d9e
Josue David Zea Herrera <69376961+Josue-Zea@users.noreply.github.com>
2021-12-08 11:27:51 -0600

    Corrección de tipos

--- Interprete/Expresions/Aritmetica.js
+++ Interprete/Expresions/Aritmetica.js
@@ -17,181 +17,181 @@ class Aritmetica extends Instruction{
       if (this.operator === ARITMETIC_OPERATOR.SUM){
         if (this.operLeft.type === Type.INT && this.operRight.type === Type.INT){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) + this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) + this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type == Type.INT && this.operRight.type == Type.DOUBLE || this.operLeft.type == Type.DOUBLE && this.operRight.type == Type.INT){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) + this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) + this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type == Type.INT && this.operRight.type == Type.CHAR || this.operLeft.type == Type.CHAR && this.operRight.type == Type.INT){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) + this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) + this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type == Type.DOUBLE && this.operRight.type == Type.DOUBLE ){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) + this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) + this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type == Type.DOUBLE && this.operRight.type == Type.CHAR || this.operLeft.type == Type.CHAR && this.operRight.type == Type.DOUBLE){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) + this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) + this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type == Type.CHAR && this.operRight.type == Type.CHAR ){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) + this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) + this.getVal(this.operRight.type, right);
         }
         return Exception("Semantico", "Tipos erroneos en operación suma.", this.row, this.column);
       
       }else if (this.operator === ARITMETIC_OPERATOR.REST){
         if (this.operLeft.type === Type.INT && this.operRight.type === Type.INT){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) - this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) - this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.INT && this.operRight.type === Type.DOUBLE || this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.INT){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) - this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) - this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.INT && this.operRight.type === Type.CHAR || this.operLeft.type === Type.CHAR && this.operRight.type === Type.INT){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) - this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) - this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.DOUBLE ){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) - this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) - this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.CHAR || this.operLeft.type === Type.CHAR && this.operRight.type === Type.DOUBLE){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) - this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) - this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.CHAR && this.operRight.type === Type.CHAR ){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) - this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) - this.getVal(this.operRight.type, right);
         }
         return Exception("Semantico", "Tipos erroneos en operación resta.", this.row, this.column);
 
       }else if (this.operator === ARITMETIC_OPERATOR.MULT){
         if (this.operLeft.type === Type.INT && this.operRight.type === Type.INT){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) * this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) * this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.INT && this.operRight.type === Type.DOUBLE || this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.INT){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) * this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) * this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.INT && this.operRight.type === Type.CHAR || this.operLeft.type === Type.CHAR && this.operRight.type === Type.INT){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) * this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) * this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.DOUBLE){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) * this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) * this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.CHAR || this.operLeft.type === Type.CHAR && this.operRight.type === Type.DOUBLE){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) * this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) * this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.CHAR && this.operRight.type === Type.CHAR){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) * this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) * this.getVal(this.operRight.type, right);
         }
         return Exception("Semantico", "Tipos erroneos en operación multiplicación.", this.row, this.column);
 
       }else if (this.operator === ARITMETIC_OPERATOR.DIV){
         if (this.operLeft.type === Type.INT && this.operRight.type === Type.INT){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) / this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) / this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.INT && this.operRight.type === Type.DOUBLE || this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.INT){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) / this.returnVal(this.operRight.type, right)
+          return this.getVal(this.operLeft.type, left) / this.getVal(this.operRight.type, right)
         }else if (this.operLeft.type === Type.INT && this.operRight.type === Type.CHAR || this.operLeft.type === Type.CHAR && this.operRight.type === Type.INT){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) / this.returnVal(this.operRight.type, right)
+          return this.getVal(this.operLeft.type, left) / this.getVal(this.operRight.type, right)
         }else if (this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.DOUBLE){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) / this.returnVal(this.operRight.type, right)
+          return this.getVal(this.operLeft.type, left) / this.getVal(this.operRight.type, right)
         }else if (this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.CHAR || this.operLeft.type === Type.CHAR && this.operRight.type === Type.DOUBLE){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) / this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) / this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.CHAR && this.operRight.type === Type.CHAR){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) / this.returnVal(this.operRight.type, right)
+          return this.getVal(this.operLeft.type, left) / this.getVal(this.operRight.type, right)
         }
         return Exception("Semantico", "Tipos erroneos en operación división.", this.row, this.column);
         
       }else if (this.operator === ARITMETIC_OPERATOR.POT){
         if (this.operLeft.type == Type.INT && this.operRight.type == Type.INT){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) ^ this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) ^ this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type == Type.INT && this.operRight.type == Type.DOUBLE || this.operLeft.type == Type.DOUBLE && this.operRight.type == Type.INT){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) ^ this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) ^ this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type == Type.INT && this.operRight.type == Type.CHAR || this.operLeft.type == Type.CHAR && this.operRight.type == Type.INT){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) ^ this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) ^ this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type == Type.DOUBLE && this.operRight.type == Type.DOUBLE){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) ^ this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) ^ this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type == Type.DOUBLE && this.operRight.type == Type.CHAR || this.operLeft.type == Type.CHAR && this.operRight.type == Type.DOUBLE){
           this.type = Type.DOUBLE;
-          return this.returnVal(this.operLeft.type, left) ^ this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) ^ this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type == Type.CHAR && this.operRight.type == Type.CHAR){
           this.type = Type.INT;
-          return this.returnVal(this.operLeft.type, left) ^ this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) ^ this.getVal(this.operRight.type, right);
         }
         return Exception("Semantico", "Tipos erroneos en operación potencia.", this.row, this.column);
         
       }else if (this.operator === ARITMETIC_OPERATOR.MOD){
         if (this.operLeft.type === Type.INT && this.operRight.type === Type.INT){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) % this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) % this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.INT && this.operRight.type === Type.DOUBLE || this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.INT){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) % this.returnVal(this.operRight.type, right)
+          return this.getVal(this.operLeft.type, left) % this.getVal(this.operRight.type, right)
         }else if (this.operLeft.type === Type.INT && this.operRight.type === Type.CHAR || this.operLeft.type === Type.CHAR && this.operRight.type === Type.INT){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) / this.returnVal(this.operRight.type, right)
+          return this.getVal(this.operLeft.type, left) / this.getVal(this.operRight.type, right)
         }else if (this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.DOUBLE){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) % this.returnVal(this.operRight.type, right)
+          return this.getVal(this.operLeft.type, left) % this.getVal(this.operRight.type, right)
         }else if (this.operLeft.type === Type.DOUBLE && this.operRight.type === Type.CHAR || this.operLeft.type === Type.CHAR && this.operRight.type === Type.DOUBLE){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) % this.returnVal(this.operRight.type, right);
+          return this.getVal(this.operLeft.type, left) % this.getVal(this.operRight.type, right);
         }else if (this.operLeft.type === Type.CHAR && this.operRight.type === Type.CHAR){
           this.type = Type.DOUBLE;
-          if (this.returnVal(this.operRight.type, right) === 0){
+          if (this.getVal(this.operRight.type, right) === 0){
             return Exception("Semantico", "No es posible dividir entre cero", this.row, this.column);
           }
-          return this.returnVal(this.operLeft.type, left) % this.returnVal(this.operRight.type, right)
+          return this.getVal(this.operLeft.type, left) % this.getVal(this.operRight.type, right)
         }
         return Exception("Semantico", "Tipos erroneos en operación división.", this.row, this.column);
         
       }else if (this.operator === ARITMETIC_OPERATOR.UMENOS){
         if (this.operLeft.type == Type.INT){
           this.type = Type.INT;
-          return - this.returnVal(this.operLeft.type, left);
+          return - this.getVal(this.operLeft.type, left);
         }else if (this.operLeft.type == Type.DOUBLE){
           this.type = Type.DOUBLE;
-          return - this.returnVal(this.operLeft.type, left);
+          return - this.getVal(this.operLeft.type, left);
         }else if (this.operLeft.type == Type.CHAR){
           this.type = Type.INT;
-          return - this.returnVal(this.operLeft.type, left);
+          return - this.getVal(this.operLeft.type, left);
         }
         return Exception("Semantico", "Acción erronea en operator unario -.", this.row, this.column);
       }
@@ -200,12 +200,12 @@ class Aritmetica extends Instruction{
     }
   }
 
-  returnVal(type, value){
+  getVal(type, value){
     if (type === Type.INT){
       return parseInt(value);
     }else if( type === Type.DOUBLE){
       return parseFloat(value);
-    }else if(type === Type.BOOLEANO){
+    }else if(type === Type.BOOLEAN){
       return Boolean(value);
     }else if(type === Type.CHAR){
       return parseInt(value.charCodeAt(0));
